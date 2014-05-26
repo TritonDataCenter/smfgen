5ada6796836a778485fb88b0ff0725c49a275d3c
Dave Eddy <dave@daveeddy.com>
2014-05-26 12:33:53 -0400

    add API for programmatic smfgen

---
+++ test/module-test.js
@@ -0,0 +1,18 @@
+var assert = require('assert');
+var fs = require('fs');
+var path = require('path');
+var smfgen = require('../');
+
+// test data from the filesystem, pregenerated
+var json = require(path.join(__dirname, 'manifest.json'));
+var xml = fs.readFileSync(path.join(__dirname, 'manifest.xml'), 'utf-8');
+
+// stream smfgen data into a buffer
+var generatedManifest = '';
+var fakestream = {
+  write: function(a) { generatedManifest += a; }
+};
+
+smfgen(fakestream, json);
+
+assert.strictEqual(generatedManifest, xml);
